85e37285aff19b977c3ac1c289a21d275d47a1c9
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2017-05-16 22:04:55 +0900

    Fix bug

--- backup/daily.sh
+++ backup/daily.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for server in `cat servers`; do
+for server in `cat $(dirname "$0")/servers`; do
     echo $server
-    ssh $server "cd /root/server-public/cn; git pull; ./daily.sh"
+    ssh $server "cd /root/server-public; git pull; cd cn; ./daily.sh"
 done
--- backup/init.sh
+++ backup/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for server in `cat servers`; do
+for server in `cat $(dirname "$0")/servers`; do
     echo $server
     ssh $server git clone https://github.com/snu-sf/server-public.git /root/server-public
 done
