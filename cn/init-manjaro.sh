2162fbe87fa905188567e2f502847cf5747a1c84
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2017-05-17 11:58:50 +0900

    Fix init-manjaro.sh

--- cn/init-manjaro.sh
+++ cn/init-manjaro.sh
@@ -9,8 +9,8 @@ systemctl enable sshd
 systemctl start sshd
 
 # get NFS share
-mkdir /web
-mkdir /opt/devel
+mkdir -p /web
+mkdir -p /opt/devel
 echo "sn01-external:/data/home  /home       nfs     rsize=8192,wsize=8192,timeo=14,intr" >> /etc/fstab
 echo "sn01-external:/data/web  /web       nfs     rsize=8192,wsize=8192,timeo=14,intr" >> /etc/fstab
 mount /home
@@ -19,8 +19,8 @@ mount /web
 # set root's authorized_keys
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
-cp ../authorized_keys /root/.ssh
+cp authorized_keys /root/.ssh
 chmod 600 /root/.ssh/authorized_keys
 
 # add users
-./useradd-gen.py | bash -s
+./useradd-gen.py manjaro | bash -s
