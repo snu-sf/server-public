7e04e7a60231d422af8ee69053d147f61f9071ea
Juneyoung Lee <aqjune@gmail.com>
2020-12-17 21:26:13 +0900

    remove backup dir

--- backup/daily.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for server in `cat $(dirname "$0")/servers`; do
-    echo $server
-    ssh $server "cd /root/server-public; git pull; cd cn; ./daily.sh"
-done
--- backup/init.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for server in `cat $(dirname "$0")/servers`; do
-    echo $server
-    ssh $server git clone https://github.com/snu-sf/server-public.git /root/server-public
-done
